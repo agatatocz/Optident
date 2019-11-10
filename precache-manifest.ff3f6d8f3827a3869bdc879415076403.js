b64f0eae19dea92a6c7aeeed49d1a3ad2f19a75e
Agata <agatatocz@gmail.com>
2019-11-11 00:32:41 +0100

    Updates

---
+++ precache-manifest.ff3f6d8f3827a3869bdc879415076403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8477706a8beae203c10ecf9d555bbc85",
+    "url": "/optident/index.html"
+  },
+  {
+    "revision": "8e82d9dc03d9b2d1e9c1",
+    "url": "/optident/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "69ad36d443020d83f335",
+    "url": "/optident/static/js/2.c8f26bba.chunk.js"
+  },
+  {
+    "revision": "8e82d9dc03d9b2d1e9c1",
+    "url": "/optident/static/js/main.e6215aa0.chunk.js"
+  },
+  {
+    "revision": "9ec7594769ea8cb1ed9f",
+    "url": "/optident/static/js/runtime-main.de682038.js"
+  }
+]);
